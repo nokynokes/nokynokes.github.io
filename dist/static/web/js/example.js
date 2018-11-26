e45ea59e8e2180da0001889f4956caa6c1a25508
nokynokes <nokynokes@gmail.com>
2018-11-26 09:44:55 -0800

    delete everything

--- dist/static/web/js/example.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log("Hello World!");
-
-document.getElementsByTagName('h1')[1].innerHTML = "Hello World!";
-document.getElementById('hi').innerHTML = "Hello JS!";
-
-document.getElementById('hi').className +="bigredhead";
